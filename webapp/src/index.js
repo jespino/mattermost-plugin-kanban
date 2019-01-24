4167289c0ad9f8c1fe73569c613cb12a00b015a3
Jesús Espino <jespinog@gmail.com>
2019-01-24 22:11:09 +0100

    Better active and show handlers in channel header

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {getCurrentChannel} from 'mattermost-redux/selectors/entities/channels';
+
 import {id as pluginId} from './manifest';
 
 import reducer from './reducer';
@@ -8,14 +10,13 @@ import {BOARD_CHANGED} from './action_types';
 
 import Kanban from './kanban';
 import KanbanAppLink from './kanban_app_link';
-import ChannelHeaderIcon from './channel_header_icon';
 
 export default class Plugin {
     initialize(registry, store, browserHistory) {
-        const state = store.getState();
         registry.registerChannelHeaderButtonAction(
-            <ChannelHeaderIcon/>,
+            <i className='fa fa-trello'/>,
             (channel) => {
+                const state = store.getState();
                 if (browserHistory.location.pathname === '/' + state.entities.teams.teams[channel.team_id].name + '/channel-apps/' + channel.name + '/kanban') {
                     browserHistory.push('/' + state.entities.teams.teams[channel.team_id].name + '/channels/' + channel.name);
                 } else {
@@ -24,6 +25,12 @@ export default class Plugin {
             },
             'Channel Kanban Board',
             'Channel Kanban Board',
+            () => true,
+            () => {
+                const state = store.getState();
+                const channel = getCurrentChannel(state);
+                return browserHistory.location.pathname === '/' + state.entities.teams.teams[channel.team_id].name + '/channel-apps/' + channel.name + '/kanban';
+            }
         );
         registry.registerTeamAppComponent(KanbanAppLink);
         registry.registerAppCenterComponent('kanban', Kanban);
